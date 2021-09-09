11b0ec7b82dd2b2c974ebefe37a9499ab873c85c
erickv <erickv@bluetrailsoft.com>
2021-09-09 18:07:46 -0500

    Loading page

---
+++ client/src/pages/Loading.js
@@ -0,0 +1,16 @@
+import logo from "../spotify-logo-png-7053.png";
+import "../App.css";
+
+const Loading = () => {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        We're setting all up for you...
+        <br />
+      </p>
+    </header>
+  );
+};
+
+export default Loading;
