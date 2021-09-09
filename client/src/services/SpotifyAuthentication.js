cab9e6066b7802a65dfbe64d46053d11877499ca
erickv <erickv@bluetrailsoft.com>
2021-09-09 18:09:38 -0500

    added getToken resource

--- client/src/services/SpotifyAuthentication.js
+++ client/src/services/SpotifyAuthentication.js
@@ -4,24 +4,15 @@ import constants from "../lib/constants";
 
 // Spotify OAuth Service
 // axios instance
-const instance = axios.create({
+const spotifyInstance = axios.create({
   baseURL: constants.spotifyOAuthUrl,
   timeout: 5000,
 });
 
-const SpotifyOAuthService = {
-  getSpotifyLogin: async function () {
-    const options = {
-      params: {
-        client_id: constants.spotifyClientId,
-        redirect_uri: `${constants.serverUrl}${constants.spotifyRedirectEndpoint}`,
-        scope: constants.spotifyOAuthScopes,
-        response_type: "token",
-      },
-    };
-
-    return instance.get(constants.spotifyAuthorizeEP, options).then().catch();
+const ApiService = {
+  getAccessToken: async function () {
+    return axios.get(`${constants.serverUrl}${constants.serverTokenPath}`);
   },
 };
 
-export default SpotifyOAuthService;
+export default ApiService;
