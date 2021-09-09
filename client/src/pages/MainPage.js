a89fc9fa0e3369833317f526c7802e00da341ef3
erickv <erickv@bluetrailsoft.com>
2021-09-09 18:08:59 -0500

    lazy loaded main page fetching for token

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,53 +1,18 @@
-import logo from "../spotify-logo-png-7053.png";
+import { useState } from "react";
 import "../App.css";
 
-// Querystring formatter
-import qs from "querystring";
+// ApiService
+import ApiService from "../services/SpotifyAuthentication";
 
-// Spotify Authorize URL constants
-import constants from "../lib/constants";
+// Custom hooks
+import useTokenEffect from "../hooks/useTokenEffect";
 
-// UI Material Components
-import { Button } from "@material-ui/core";
+const MainPage = () => {
+  const [token, setToken] = useState("");
 
-function MainPage() {
-  const loginRedirectCallback = () => {
-    const authorizeUrl =
-      `${constants.spotifyOAuthUrl}${constants.spotifyAuthorizeEP}?`.concat(
-        qs.stringify({
-          client_id: constants.spotifyClientId,
-          redirect_uri: constants.spotifyRedirectUri,
-          response_type: "code",
-          scopes: constants.spotifyOAuthScopes,
-        })
-      );
-    window.open(authorizeUrl);
-  };
+  useTokenEffect(token, setToken);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Spotify personal dashboard.
-          <br />
-          <sub>
-            {" "}
-            by <a href="https://github.com/erickvneri">erickvneri</a>
-          </sub>
-        </p>
-
-        <Button
-          color={"primary"}
-          onClick={loginRedirectCallback}
-          size={"medium"}
-          variant={"contained"}
-        >
-          Login
-        </Button>
-      </header>
-    </div>
-  );
-}
+  return <header className="App-header">{token}</header>;
+};
 
 export default MainPage;
