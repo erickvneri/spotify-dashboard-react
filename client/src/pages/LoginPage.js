c7599ef43fb260f9a1f1396578787886515dcd79
erickv <erickv@bluetrailsoft.com>
2021-09-09 18:08:02 -0500

    renamed MainPage to LoginPage

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,51 @@
+import logo from "../spotify-logo-png-7053.png";
+import "../App.css";
+
+// Querystring formatter
+import qs from "querystring";
+
+// Spotify Authorize URL constants
+import constants from "../lib/constants";
+
+// UI Material Components
+import { Button } from "@material-ui/core";
+
+function LoginPage() {
+  const loginRedirectCallback = () => {
+    const authorizeUrl =
+      `${constants.spotifyOAuthUrl}${constants.spotifyAuthorizeEP}?`.concat(
+        qs.stringify({
+          client_id: constants.spotifyClientId,
+          redirect_uri: constants.spotifyRedirectUri,
+          response_type: "code",
+          scopes: constants.spotifyOAuthScopes,
+        })
+      );
+    window.open(authorizeUrl);
+  };
+
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Spotify personal dashboard.
+        <br />
+        <sub>
+          {" "}
+          by <a href="https://github.com/erickvneri">erickvneri</a>
+        </sub>
+      </p>
+
+      <Button
+        color={"primary"}
+        onClick={loginRedirectCallback}
+        size={"medium"}
+        variant={"contained"}
+      >
+        Login
+      </Button>
+    </header>
+  );
+}
+
+export default LoginPage;
