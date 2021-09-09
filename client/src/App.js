7689aff1c67c151ca67172a81905939080c5fa66
erickv <erickv@bluetrailsoft.com>
2021-09-09 18:08:35 -0500

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import logo from "./spotify-logo-png-7053.png";
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 // Pages
+import LoginPage from "./pages/LoginPage";
+import Loading from "./pages/Loading";
 import MainPage from "./pages/MainPage";
 
 function App() {
-  return <MainPage />;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact strict path="/">
+            <Page children={<LoginPage />}></Page>
+          </Route>
+
+          <Route exact strict path="/oauth">
+            <Page fallback={<Loading />}>{<LazyPage />}</Page>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
+
+function Page({ children, fallback }) {
+  return (
+    <div className="Page">
+      <Suspense fallback={fallback || null}>{children}</Suspense>
+    </div>
+  );
+}
+
+const LazyPage = lazy(
+  () =>
+    new Promise((resolve, reject) =>
+      setTimeout(() => resolve(import("./pages/MainPage")), 3 * 1000)
+    )
+);
 
 export default App;
